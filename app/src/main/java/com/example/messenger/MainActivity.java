2e504de5ac764542a5a2028264503e6fb7eb8850
ndphong812 <ndphong812@gmail.com>
2022-10-08 21:12:33 +0700

    update login ui - https://ndphong.atlassian.net/jira/software/projects/A2G/boards/1

--- app/src/main/java/com/example/messenger/MainActivity.java
+++ app/src/main/java/com/example/messenger/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.messenger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
     }
