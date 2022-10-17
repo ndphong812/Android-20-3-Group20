9886a7003a2f8d3cd4925f7c929bc6a518536dc9
Phạm Quốc Thái <pqthai2502@gmail.com>
2022-10-17 15:17:27 +0700

    Revert "https://ndphong.atlassian.net/browse/A2G-3"
    This reverts commit 12602a2a7422866111c4f7acfec8aae46b61c86f, reversing
    changes made to 2c5abc2dde3ea5ac918b9944c70d47373262468f.

--- app/src/main/java/com/example/messenger/SettingsActivity.java
+++ app/src/main/java/com/example/messenger/SettingsActivity.java
@@ -1,28 +1,20 @@
 package com.example.messenger;
 
-import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO;
-import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_UNSPECIFIED;
-import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES;
-
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.provider.Settings;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
+public class SettingsActivity extends Activity {
 
-public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.layout_settings);
+
         int itemHeight = 165;
 
         ListView firstListView = findViewById(R.id.firstListView);
@@ -56,28 +48,8 @@ public class SettingsActivity extends AppCompatActivity {
         firstListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                if (i==0){
-                    if(AppCompatDelegate.getDefaultNightMode()==MODE_NIGHT_UNSPECIFIED)
-                    {
-                        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES);
-                        Toast.makeText(SettingsActivity.this, "Chế độ tối đang bật", Toast.LENGTH_LONG).show();
-                    }
-                    else if (AppCompatDelegate.getDefaultNightMode()==MODE_NIGHT_NO)
-                    {
-                        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_YES);
-                        Toast.makeText(SettingsActivity.this, "Chế độ tối đang bật", Toast.LENGTH_LONG).show();
-
-                    }
-                    else
-                    {
-                        AppCompatDelegate.setDefaultNightMode(MODE_NIGHT_NO);
-                        Toast.makeText(SettingsActivity.this, "Chế độ tối đang tắt", Toast.LENGTH_LONG).show();
-
-                    };
-                }else{
-                    Toast.makeText(SettingsActivity.this, "This part is not available for now!", Toast.LENGTH_LONG).show();
-                }}
-
+                Toast.makeText(SettingsActivity.this, "This part is not available for now!", Toast.LENGTH_LONG).show();
+            }
         });
 
         secondListView.setAdapter(secondAdapter);
@@ -85,7 +57,6 @@ public class SettingsActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Toast.makeText(SettingsActivity.this, "This part is not available for now!", Toast.LENGTH_LONG).show();
-
             }
         });
 
@@ -104,6 +75,6 @@ public class SettingsActivity extends AppCompatActivity {
                 Toast.makeText(SettingsActivity.this, "This part is not available for now!", Toast.LENGTH_LONG).show();
             }
         });
-
     }
 }
+
